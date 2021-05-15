adac4a1cf5049582daf1e00326ecf718e1fe851c
Shinwaffle <59802882+Shinwaffle@users.noreply.github.com>
2021-05-14 19:17:51 -0500

    part 1 and 2 done

--- src/PongGame.java
+++ src/PongGame.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class PongGame extends JPanel implements KeyListener,
         ActionListener, Runnable {
@@ -30,7 +32,10 @@ public class PongGame extends JPanel implements KeyListener,
     Rectangle Ball = new Rectangle(ballx, bally, 5, 5);
     Rectangle Bat = new Rectangle(batx, baty, 40, 5);
 
-    Rectangle[] Brick = new Rectangle[12];
+    int lives = 3; //default amount of lives
+    int amount = 12; //change this value to update amount of bricks
+    Rectangle[] Brick = new Rectangle[amount];
+    ArrayList<ArrayList<Float>> color = new ArrayList<>();
     Thread t;
 
     public PongGame() {
@@ -60,6 +65,8 @@ public class PongGame extends JPanel implements KeyListener,
     // declaring ball, paddle,bricks
     @Override
     public void paint(Graphics g) {
+        Font f = new Font("Arial", Font.BOLD, 10);
+        g.setFont(f);
         //draw play area
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 0, 350, 450);
@@ -70,15 +77,33 @@ public class PongGame extends JPanel implements KeyListener,
         g.setColor(Color.red);
         g.drawRect(0, 0, 343, 250);
         //draw ball (Task 1)
+        g.setColor(Color.blue);
+        g.fillOval(Ball.x, Ball.y, Ball.width, Ball.height);
         //draw paddle (Task 2)
+        g.fill3DRect(Bat.x, Bat.y, Bat.width, Bat.height, true);
         //paint brick (Task 3)
+        for (int i = 0; i < Brick.length; i++) { //added color adding code
+            if (Brick[i] != null) {
+                ArrayList<Float> colorIndex = color.get(i);
+                g.setColor(new Color(colorIndex.get(0), colorIndex.get(1), colorIndex.get(2)));
+                g.fill3DRect(Brick[i].x, Brick[i].y, Brick[i].width,
+                        Brick[i].height, true);
+            }
+        }
+        g.setColor(Color.white);
+        /*
+        I think there's some performance hits by concatenating the string so much but whatever
+         */
+        g.drawString("Bricks remaining: " + (Brick.length-count), 40, 300);
+        g.drawString("Score: " + count, 40, 350);
+        g.drawString("Paddles remaining: " + lives, 40, 325);
 
         if (ballFallDown || bricksOver) {
-            Font f = new Font("Arial", Font.BOLD, 20);
+            f = new Font("Arial", Font.BOLD, 20);
             g.setFont(f);
             g.drawString(status, 70, 120);
-            ballFallDown = false;
-            bricksOver = false;
+            ballFallDown = true;
+            bricksOver = true;
         }
 
     }
@@ -106,22 +131,41 @@ public class PongGame extends JPanel implements KeyListener,
         // ====================================================
         // == ball reverses when touches the brick=======
 
-        while (ballFallDown && bricksOver ) {
+        while (ballFallDown == false && bricksOver == false) { //replaced the "== false" statements with ! because, why honestly
         // routine to create collision  (Task 4)
-
             if (start) {
                 // /////////// =================================
 
                 if (count == Brick.length) {// check if ball hits all bricks
-                    bricksOver = true;
                     status = "YOU WON THE GAME";
+                    bricksOver = true;
                     repaint();
                 }
                 // /////////// =================================
                 repaint();
                 Ball.x += movex;
                 Ball.y += movey;
+                for (int i = 0; i < Brick.length; i++) {
+                    if (Brick[i] != null && Brick[i].intersects(Ball)) {
+                            Brick[i] = null;
+                            movey = -movey;
+                            count++;
 
+                    }
+                } //250
+                if (Ball.y >= 250) {
+                    if (lives != 0) {
+                        lives--;
+                        ballx = 160;
+                        bally = 150;
+                        movey = -movey;
+                        movex = -movex;
+                    } else {
+                        status = "YOU LOST THE GAME";
+                        ballFallDown = true;
+                        repaint();
+                    }
+                }
                 if (left) {
 
                     Bat.x -= 5;
@@ -139,7 +183,6 @@ public class PongGame extends JPanel implements KeyListener,
                 // /===== Ball reverses when strikes the bat
                 if (Ball.intersects(Bat)) {
                     movey = -movey;
-                    // if(Ball.y + Ball.width >=Bat.y)
                 }
                 // //=====================================
                 // ....ball reverses when touches left and right boundary
@@ -153,7 +196,7 @@ public class PongGame extends JPanel implements KeyListener,
 
             }
             try {
-                Thread.sleep(16); //change refresh speed
+                Thread.sleep(8); //change refresh speed
             } catch (Exception ex) {
             }// try catch ends here
 
@@ -202,8 +245,23 @@ public class PongGame extends JPanel implements KeyListener,
     }
 
     //Method to paint your brick pattern (Task 6)
-    public void paintBrick() {
+    public void paintBrick() { //modified to scale, also assign colors once over the course of the program
+        int index = 5;
+        for (int i = 0; i < Brick.length; i++) {
+            ArrayList<Float> colorIndex = new ArrayList<>();
+            colorIndex.add((float) Math.random());
+            colorIndex.add((float) Math.random());
+            colorIndex.add((float) Math.random());
+            color.add(colorIndex);
+            Brick[i] = new Rectangle(brickx, bricky, 30, 10);
+            if (i == index) {
+                brickx = 39;
+                bricky += 12;
+                index = index + 6;
+            }
 
+            brickx += 31;
+        }
     }
 
     public void restart() {
@@ -229,7 +287,7 @@ public class PongGame extends JPanel implements KeyListener,
         Ball = new Rectangle(ballx, bally, 5, 5);
         Bat = new Rectangle(batx, baty, 40, 5);
         // Rectangle Brick;// = new Rectangle(brickx, bricky, 30, 10);
-        Brick = new Rectangle[12];
+        Brick = new Rectangle[amount];
 
         movex = -1;
         movey = -1;
